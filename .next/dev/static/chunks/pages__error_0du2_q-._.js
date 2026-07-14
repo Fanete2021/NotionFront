b395d5d59abb224542324c9cfa70ae53e52f45fd
Grisha55 <grishavinyar@icloud.com>
2026-07-14 12:46:47 +0300

    Удалил папку .next и добавил ее в gitignore

--- .next/dev/static/chunks/pages__error_0du2_q-._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/0-cn_next_dist_compiled_17galxi._.js","static/chunks/0-cn_next_dist_shared_lib_1bfz_55._.js","static/chunks/0-cn_next_dist_client_1ux_m3p._.js","static/chunks/0-cn_next_dist_1sl5tn-._.js","static/chunks/[next]_entry_page-loader_ts_0shuob4._.js","static/chunks/162a_react-dom_1hsf_h5._.js","static/chunks/node_modules__pnpm_20tl2_t._.js","static/chunks/[root-of-the-server]__02sxxph._.js"],
-    source: "entry"
-});
--- .next/dev/static/chunks/src_app_page_tsx_19vsip8._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/0-cn_next_dist_097wej_._.js","static/chunks/src_app_page_module_0cxh65o.css"],
-    source: "dynamic"
-});
