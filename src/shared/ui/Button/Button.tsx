9c0eeb8dff987c5bf1ff8315d5cb92817e3f59d3
Hikaru <nikita.belyaev.05@mail.ru>
2026-07-15 13:28:16 +0300

    Добавил storybook, сделал пример работы на кнопке

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,15 +1,31 @@
 "use client"; // <-- Эта строка обязательна для интерактивных компонентов
 
 import styles from './Button.module.css';
+import React from "react";
 
-interface ButtonProps {
-  children: React.ReactNode;
-  onClick?: () => void;
-}
+type ButtonProps = React.ComponentProps<"button"> & {
+  variant?: 'primary' | 'secondary';
+  size?: 'xs' | 's' | 'md' | 'lg';
+  children?: React.ReactNode;
+};
 
-export const Button = ({ children, onClick }: ButtonProps) => {
+export const Button = ({
+   children,
+   className,
+   variant = 'primary',
+   size = 'md',
+   onClick,
+   ...props
+ }: ButtonProps) => {
+  const buttonClasses = [
+    styles.button,
+    styles[variant],
+    styles[size],
+    className
+  ].filter(Boolean).join(' ');
+
   return (
-    <button className={styles.button} onClick={onClick}>
+    <button className={buttonClasses} onClick={onClick} {...props}>
       {children}
     </button>
   );
