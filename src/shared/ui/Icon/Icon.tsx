d8050a9c7497a4eeb4ffc26d8f2275d38291893a
Grisha55 <grishavinyar@icloud.com>
2026-07-17 22:41:10 +0300

    Сделал ui и stories для Icon

---
+++ src/shared/ui/Icon/Icon.tsx
@@ -0,0 +1,51 @@
+import cn from "classnames";
+import styles from "./Icon.module.scss";
+
+type SvgProps = Omit<React.SVGProps<SVGSVGElement>, "onClick">;
+
+interface IconBaseProps extends SvgProps {
+    className?: string;
+    Svg: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+}
+
+interface NonClickableIconProps extends IconBaseProps {
+    clickable?: false;
+}
+
+interface ClickableBaseProps extends IconBaseProps {
+    clickable: true;
+    onClick: () => void;
+}
+
+type IconProps = NonClickableIconProps | ClickableBaseProps;
+
+export const Icon = (props: IconProps) => {
+    const {
+        className,
+        Svg,
+        width = 32,
+        height = 32,
+        clickable,
+        ...otherProps
+    } = props;
+
+    const icon = (
+        <Svg
+            className={cn(styles.icon, {}, [className])}
+            width={width}
+            height={height}
+            {...otherProps}
+            onClick={undefined}
+        />
+    );
+
+    if (clickable) {
+        return (
+            <button type="button" className={styles.button} onClick={props.onClick} style={{ height, width }}>
+                {icon}
+            </button>
+        )
+    }
+
+    return icon;
+};
