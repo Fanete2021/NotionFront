fbc8ecebb34b58b00b3bc68eb0d18218b4b59a33
Hikaru <nikita.belyaev.05@mail.ru>
2026-07-19 12:44:41 +0300

    Добавил компонент Checkbox

---
+++ src/shared/ui/Checkbox/Checkbox.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+
+import { Checkbox } from './Checkbox';
+
+const meta = {
+  title: 'Checkbox',
+  component: Checkbox,
+  tags: ['autodocs'],
+  args: {
+    children: 'Accept terms and conditions',
+  },
+} satisfies Meta<typeof Checkbox>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const Checked: Story = {
+  args: {
+    defaultChecked: true,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
+
+export const DisabledChecked: Story = {
+  args: {
+    disabled: true,
+    defaultChecked: true,
+  },
+};
