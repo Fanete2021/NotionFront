403316108a160751decee4a90426dd8ce565b044
Grisha55 <grishavinyar@icloud.com>
2026-08-05 21:08:52 +0300

    Deleted comments

--- src/pages/Home/index.ts
+++ src/pages/Home/index.ts
@@ -1,6 +1,5 @@
 export { HomePage } from './Home';
 
-// Next.js App Router поддерживает экспорт metadata из дочерних компонентов
 export const metadata = {
   title: 'Главная страница | FSD Next.js',
   description: 'Базовая структура Feature-Sliced Design для Next.js',
