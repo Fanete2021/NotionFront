0003854461dd6ec2cfe2a74ee7e509479eb2615e
Hikaru <nikita.belyaev.05@mail.ru>
2026-08-17 16:26:13 +0300

    [NOT-36] fix: Удалил код для тестирования поведения отображения error.tsx

--- src/features/projects/sharing/ShareButton.tsx
+++ src/features/projects/sharing/ShareButton.tsx
@@ -1,25 +1,14 @@
-'use client';
-
-import { useTransition } from 'react';
 import styles from './ShareButton.module.css';
 import { Button } from '@shared/ui/Button';
 import ShareIcon from '@shared/assets/icons/share.svg';
 
 export const ShareButton = () => {
-  const [, startTransition] = useTransition();
-
-  const handleClick = () =>
-    startTransition(() => {
-      throw new Error('Internal Server Error');
-    });
-
   return (
     <Button
       type="button"
       variant="outline"
       size="sm"
       className={styles.button}
-      onClick={handleClick}
       addonLeft={<ShareIcon className={styles.icon} />}
     >
       Поделиться
