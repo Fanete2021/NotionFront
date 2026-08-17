ac784cab823a8db09e53e0c8a7da86e9d2daeeac
Hikaru <nikita.belyaev.05@mail.ru>
2026-08-17 16:19:21 +0300

    [NOT-36] fix: Исправил иморт файла глобальных стилей

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from 'react';
-// eslint-disable-next-line fsd/no-relative-imports
-import styles from './layout.module.css';
+import styles from '@/app/layout.module.css';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return <section className={styles.container}>{children}</section>;
