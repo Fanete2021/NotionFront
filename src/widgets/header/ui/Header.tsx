c4e6ec9a4bc9ad0412c3ff8e4a6b09524069491d
Grisha55 <grishavinyar@icloud.com>
2026-08-23 03:58:24 +0300

    not-33/ Added header component files

---
+++ src/widgets/header/ui/Header.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import styles from './Header.module.css';
+import { Button } from '@shared/ui/Button';
+
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h2 className={styles.title}>Наш Notion Проект</h2>
+      <Button onClick={() => alert('Работает!')}>Действие</Button>
+    </header>
+  );
+};
