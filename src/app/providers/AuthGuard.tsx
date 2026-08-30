9cff2ee03baf4fb994454d01b4ae68df340af982
Grisha55 <grishavinyar@icloud.com>
2026-08-30 15:55:29 +0300

    not-45/Created Loader, updated AuthGuard

--- src/app/providers/AuthGuard.tsx
+++ src/app/providers/AuthGuard.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { selectSessionStatus } from '../store';
+import { loggedOut, selectSessionStatus } from '../store';
 import { useGetMeQuery } from '@entities/user';
-import { useAppSelector } from '@/shared/lib';
+import { useAppDispatch, useAppSelector } from '@/shared/lib';
+import { Loader } from '@/shared/ui/loader';
 
 type AuthGuardProps = {
   children: React.ReactNode;
@@ -12,16 +13,38 @@ type AuthGuardProps = {
 
 export const AuthGuard = ({ children }: AuthGuardProps) => {
   const router = useRouter();
-
+  const dispatch = useAppDispatch();
   const status = useAppSelector(selectSessionStatus);
+  const [isClient, setIsClient] = useState(false);
 
-  useGetMeQuery(undefined, { skip: status !== 'unknown' });
+  const { isLoading, isError } = useGetMeQuery(undefined, {
+    skip: status !== 'unknown',
+  });
+
+  useEffect(() => {
+    // eslint-disable-next-line
+    setIsClient(true);
+  }, []);
+
+  useEffect(() => {
+    if (isError && status === 'unknown') {
+      dispatch(loggedOut());
+    }
+  }, [isError, status, dispatch]);
 
   useEffect(() => {
     if (status === 'anonymous') {
       router.replace('/login');
     }
   }, [router, status]);
+
+  if (!isClient) {
+    return <Loader />;
+  }
+
+  if (status === 'unknown' || status === 'anonymous') {
+    return <Loader />;
+  }
 
   return children;
 };
