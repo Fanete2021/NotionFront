f4f727286783c5889f3fd8ab95380263a63c46bb
kArAsTeR734 <nikita.belyaev.05@mail.ru>
2026-09-05 16:36:30 +0300

    [NOT-54] fix: Auth Guard refactoring

--- src/app/providers/AuthGuard.tsx
+++ src/app/providers/AuthGuard.tsx
@@ -1,18 +1,14 @@
 'use client';
 
-import { useEffect, type ReactNode } from 'react';
-import { usePathname, useRouter } from 'next/navigation';
-import { selectSessionStatus } from '../store';
+import { useEffect, useState, type ReactNode } from 'react';
+import { useRouter, usePathname } from 'next/navigation';
+import { loggedOut, selectSessionStatus } from '../store';
 import { NotFoundError } from '@widgets/error';
 import { useGetMeQuery } from '@entities/user';
-import { useAppSelector } from '@shared/lib';
 import { PRIVATE_ROUTES, PUBLIC_ROUTES, ROUTES } from '@shared/routes';
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { loggedOut, selectSessionStatus } from '../store';
-import { useGetMeQuery } from '@entities/user';
 import { useAppDispatch, useAppSelector } from '@/shared/lib';
 import { Loader } from '@/shared/ui/loader';
+import { toast } from '@/shared/ui/toast';
 
 type AuthGuardProps = {
   children: ReactNode;
@@ -28,13 +24,11 @@ const isPrivateRoute = (pathname: string) =>
 export const AuthGuard = ({ children }: AuthGuardProps) => {
   const router = useRouter();
   const pathname = usePathname();
-  const status = useAppSelector(selectSessionStatus);
-  useGetMeQuery(undefined, { skip: status !== 'unknown' });
   const dispatch = useAppDispatch();
   const status = useAppSelector(selectSessionStatus);
   const [isClient, setIsClient] = useState(false);
 
-  const { isLoading, isError } = useGetMeQuery(undefined, {
+  const { isError } = useGetMeQuery(undefined, {
     skip: status !== 'unknown',
   });
 
@@ -53,12 +47,25 @@ export const AuthGuard = ({ children }: AuthGuardProps) => {
   const isCurrentRoutePrivate = pathname !== null && isPrivateRoute(pathname);
 
   useEffect(() => {
-    if (status !== 'authenticated') {
+    if (status === 'anonymous' && isCurrentRoutePrivate) {
       router.replace(ROUTES.login);
+      toast.add({
+        type: 'error',
+        title: 'Ошибка перехода на страницу!',
+        description: 'Пожалуйста войдите в аккаунт или создайте новый для доступа к этой странице',
+      });
     }
   }, [isCurrentRoutePrivate, router, status]);
 
-  if (pathname === null || status === 'unknown') {
+  if (!isClient) {
+    return <Loader />;
+  }
+
+  if (status === 'unknown' || status === 'anonymous') {
+    return <Loader />;
+  }
+
+  if (pathname === null) {
     return null;
   }
 
@@ -68,14 +75,6 @@ export const AuthGuard = ({ children }: AuthGuardProps) => {
 
   if (isCurrentRoutePrivate) {
     return status === 'authenticated' ? children : null;
-  }
-  
-  if (!isClient) {
-    return <Loader />;
-  }
-
-  if (status === 'unknown' || status === 'anonymous') {
-    return <Loader />;
   }
 
   return <NotFoundError />;
