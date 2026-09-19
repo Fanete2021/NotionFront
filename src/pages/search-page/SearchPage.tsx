a5c0905be1e3fdef4732c883a05c0a067f393efb
unknown <nikita.belyaev.05@mail.ru>
2026-09-19 11:51:46 +0300

    [NOT-46] renamed SearchPage import

--- src/pages/search-page/SearchPage.tsx
+++ src/pages/search-page/SearchPage.tsx
@@ -1,5 +1,5 @@
 import styles from './SearchPage.module.css';
-import { WorkspaceSearch } from '@widgets/workspace-search';
+import { WorkspaceSearch } from '@/widgets/workspace-content-search-panel';
 
 export const SearchPage = () => {
   return (
